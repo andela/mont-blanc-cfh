5ad72dda0cbf0e1b853437747efe9fae0a466cb0
Andela Developer <delighteddell@gmail.com>
2017-10-17 11:28:30 +0100

    chore(gulp-to-grunt): remove grunt packages/article model[Finishes #151991930]

--- test/user/model.js
+++ test/user/model.js
@@ -4,7 +4,6 @@
 var should = require('should'),
     app = require('../../server'),
     mongoose = require('mongoose'),
-<<<<<<< HEAD:test/user/model.js
     User = mongoose.model('User');
 
 //Globals
@@ -14,19 +13,6 @@ var user;
 describe('<Unit Test>', function() {
     describe('Model User:', function() {
         before(function(done) {
-=======
-    User = mongoose.model('User')
-    // Article = mongoose.model('Article');
-
-//Globals
-// var user;
-// var article;
-
-//The tests
-xdescribe('<Unit Test>', function() {
-    describe('Model Article:', function() {
-        beforeEach(function(done) {
->>>>>>> chore(grunt-to-gulp): convert grunt to gulp:test/article/model.js
             user = new User({
                 name: 'Full name',
                 email: 'test@test.com',
