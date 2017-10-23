69800ae7557213efc435744a4b9bbc5d0987561b
ChidinmaOrajiaku <corajiaku96@gmail.com>
2017-10-23 17:39:40 +0100

    chore(signup): token authentication tests
    write tests to ascertain token on user sign up
    
    [Delivers #151991933]

---
+++ test/user/signup.js
@@ -0,0 +1,47 @@
+/**
+ * Module dependencies.
+ */
+const should = require('should'),
+app = require('../../server'),
+mongoose = require('mongoose'),
+User = mongoose.model('User');
+chai = require('chai');
+chaiHttp = require ('chai-http');
+
+require('dotenv').config();
+
+chai.use(chaiHttp);
+
+let user;
+
+// delete all records in User model before each test
+mongoose.model('User').collection.drop();
+
+describe('Users', () => {
+    beforeEach(()  => {
+        user = {
+            name: 'Full name',
+            email: 'testt@test.com',
+            username: 'user',
+            password: 'password'
+        };
+    });
+  describe('Users', () => {
+    it('should get token on successful sign up', (done) => {
+      testUser = Object.assign({}, user)
+      chai.request(app)
+        .post('/api/auth/signup')
+        .send(testUser)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.have.token
+          res.body.should.have.message
+          res.body.token.should.be.string;
+          res.body.message.should.equal('Successfully signed up');
+          res.should.be.json;
+          if (err) return expect(err.message);
+          done();
+        });
+    });
+  });
+});
