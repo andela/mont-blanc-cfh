81e375c0aac31e872d83bfdf31f3bf2be0001175
Andela Developer <delighteddell@gmail.com>
2017-11-01 12:38:11 +0100

    bug(gulp):fix gulp bugs [Finishes #151991930]

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 import allConfiguration from '../config/env/all';
-import developmentConfiguration from '../config/env/development.json';
-import productionConfiguration from '../config/env/production.json';
-import testConfiguration from '../config/env/test.json';
+import developmentConfiguration from '../config/env/development';
+import productionConfiguration from '../config/env/production';
+import testConfiguration from '../config/env/test';
 
 const config = Object.assign(
   { },
   allConfiguration,
-  developmentConfiguration,
-  productionConfiguration,
-  testConfiguration
+  developmentConfiguration[0],
+  productionConfiguration[0],
+  testConfiguration[0]
 );
 
 export default config;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,121 +1,120 @@
-import gulp from 'gulp';
-import sass from 'gulp-sass';
-import gulpLivereload from 'gulp-livereload';
-import nodemon from 'gulp-nodemon';
-import bower from 'gulp-bower';
-import babel from 'gulp-babel';
-import coverage from 'gulp-coverage';
-import mocha from 'gulp-mocha';
-import eslint from 'gulp-eslint';
-import gulpConnect from 'gulp-connect';
+// Import neccessary libaries
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const browserSync = require('browser-sync');
+const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
+const mocha = require('gulp-mocha');
+const bower = require('gulp-bower');
+const runSequence = require('gulp-sequence');
+const clean = require('gulp-rimraf');
+const babel = require('gulp-babel');
 
 const tasks = ['eslint', 'sass', 'transpile', 'test', 'serve'];
 
-gulp.task('serve', ['sass', 'transpile'], () => {
-  nodemon({
-    watch: ['./dist', './app', './public'],
-    script: 'dist/server.js',
-    ext: 'js html jade',
-    ignore: ['dist/config', 'node_modules/**'],
-    env: {
-      NODE_ENV: 'development'
-    }
-  });
+//  lint files in public, test and app folders
+gulp.task('eslint', () => {
+  gulp.src(['public/js/**/*.js', 'test/**/*.js', 'app/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.formatEach())
+    .pipe(eslint.failOnError());
 });
 
+// Pipe sass through gulp-sass, convert to css and drop files in dist folder
+gulp.task('sass', () => {
+  gulp.src('public/css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css/'));
+});
+
+
+// Transpile js files, transfer json files, jade and generated lib files
+gulp.task('transpile-test', ['babelify', 'transfer_json', 'transfer_jades', 'transfer_libs']);
+
+
+// Transpile test and generate report
+gulp.task('test', () => {
+  gulp.src(['dist/test/**/*.js'], {
+    read: false
+  })
+    .pipe(mocha({
+      reporter: 'spec'
+    }));
+  // .once('error', () => {
+  //   process.exit(1);
+  // })
+  // .once('end', () => {
+  //   process.exit();
+  // });
+});
+
+//  Watch files for changes
 gulp.task('watch', () => {
-  gulpLivereload.listen();
   gulp.watch('./public/**/*.scss', ['sass']);
   gulp.watch('./public/**/*.html', ['transpile']);
   gulp.watch('./public/**/*.css');
   gulp.watch('./public/**/*.js', ['eslint', 'transpile']);
 });
 
-gulp.task('connect', () => {
-  gulpConnect.server({
-    root: ['dist'],
-    port: 8000,
-    livereload: true,
+// Start server.js on port 3000 and ignore listed files
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'dist/server.js',
+    ext: 'js',
+    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+    env: {
+      PORT: 3000
+    }
   });
 });
 
-gulp.task('html', () => {
-  gulp.src('./public/**/*.html')
-    .pipe(gulpConnect.reload());
-});
-
-gulp.task('eslint', () => {
-  gulp.src(['public/js/**/*.js', 'test/**/*.js', 'app/**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.formatEach())
-    .pipe(eslint.failOnError());
-});
-
-gulp.task('sass', () => {
-  gulp.src('./public/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public'));
-});
-
+//  Specify directory for bower generated libraries
 gulp.task('bower', () => {
   bower({
-    directory: 'public/lib'
+    directory: 'dist/public/lib'
   });
 });
 
-gulp.task('public', () => {
-  gulp.src([
-    './public/**/*',
-    './css/**/*',
-    'public/views/**/*',
-    'app/views/**/*',
-    './**/*.json',
-    '!package.json',
-    '!public/js/**/*',
-    '!dist/**/*',
-    '!node_modules/**/*',
-    '!eslintrc.json',
-    '!bower.json',
-  ], {
-    base: './'
-  })
-    .pipe(gulp.dest('dist'));
+//  Clean the generated folder
+gulp.task('clean', () => {
+  gulp.src('./public/lib')
+    .pipe(clean({
+      force: true
+    }));
 });
 
+//  Move JSON files config to dis/config
+gulp.task('transfer_json', () => {
+  gulp.src('config/env/**/*.json')
+    .pipe(gulp.dest('dist/config/env'));
+});
 
-gulp.task('transpile', ['public'], () => {
-  gulp.src([
-    './**/*.js',
-    '!dist/**',
-    '!node_modules/**',
-    '!public/lib/**',
-    '!gulpfile.babel.js'
-  ])
+// Move jade files app/views to dist/app/views
+gulp.task('transfer_jades', () => {
+  gulp.src('app/views/**/*')
+    .pipe(gulp.dest('dist/app/views'));
+});
+
+// Move generated libraries public folder to dest/public
+gulp.task('transfer_libs', () => {
+  gulp.src(['public/**/*', '!public/js/**'])
+    .pipe(gulp.dest('dist/public'));
+});
+
+// Pass all js files through gulp-babel and transpile using es2015 preset
+gulp.task('babelify', () => {
+  gulp.src(['./**/*.js', '!dist/**', '!node_modules/**', '!gulpfile.babel.js', '!public/lib/**'])
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('dist'));
-  // gulpConnect.reload();
 });
 
+// Run specified tasks
+gulp.task('transpile-production', runSequence('babelify', 'transfer_json', 'transfer_jades', 'transfer_libs'));
+gulp.task('transpile', runSequence('eslint', 'babelify', 'transfer_json', 'transfer_jades', 'transfer_libs'));
+// gulp.task('install', runSequence('bower'));
+gulp.task('default', runSequence('transpile'));
 
-gulp.task('test', ['transpile'], () => {
-  gulp.src([
-    './dist/test/game/game.js',
-    './dist/test/user/model.js'
-  ], {
-    read: false
-  })
-    .pipe(coverage.instrument({
-      pattern: ['**/test*'],
-      debugDirectory: 'debug'
-    }))
-    .pipe(mocha({
-      timeout: 15000
-    }))
-    .pipe(coverage.gather())
-    .pipe(coverage.format())
-    .pipe(gulp.dest('reports'));
-});
-
-gulp.task('default', tasks);
+// Script for production
+gulp.task('production', runSequence('transpile-production', 'bower'));
