87a04df4ed3f1362da26f19eacbd7a571c0ee9fb
ChidinmaOrajiaku <corajiaku96@gmail.com>
2017-11-03 11:04:40 +0100

    bug(landing-page and signout function): fix bugs in landing page
    * change logo
    * change how-to-play-cards
    * change jquery for navbar scroll
    * add redirection to sign out method
    * fix navbar and button responsiveness
    * fix scroll for navlinks
    * link mont-blanc git hub repo
    * change navlink hover color
    * change navlink to sentence case
    * add dashboard to navlink
    
    [Delivers #151991935]

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,29 @@
-$(document).ready(() => {
-  /**
-   * Initialize AOS
-   */
-  AOS.init();
 
+$(document).ready(() => {
+  //  Initialize AOS
+  AOS.init();
 
   $(window).bind('scroll', () => {
     const scrollAmount = $(window).scrollTop();
     if (scrollAmount > 50) {
       $('.navbar-landing').addClass('navbar-scroll');
+      $('.navbar-brand-fixed').addClass('navbar-brand-animate');
+      $('.nav-link').addClass('nav-link-animate');
+      $('.nav').addClass('link-adjust');
+      $('#image').removeClass('initialLogo');
+      $('#image').addClass('finalLogo');
     } else {
       $('.navbar-landing').removeClass('navbar-scroll');
+      $('.navbar-brand-fixed').removeClass('navbar-brand-animate');
+      $('.nav-link').removeClass('nav-link-animate');
+      $('.nav').removeClass('link-adjust');
+      $('#image').addClass('initialLogo');
+      $('#image').removeClass('finalLogo');
     }
+  });
+
+  // Hide nabvar on click
+  $('nav').find('li').bind('click', 'a', function () {
+    $(this).collapse('hide');
   });
 });
