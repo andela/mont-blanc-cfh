78862a088d2c3eb1694b372f20df20b9cfcec75e
Temitope Emmanuel <temitope.emmanuel63@gmail.com>
2017-11-08 10:36:49 +0100

    feature(categorised by region): Cards should be categorised by region
    users should be able to play game based on their location
    
    [finish #151991942]

--- app/controllers/games.js
+++ app/controllers/games.js
@@ -8,17 +8,93 @@ import Game from '../models/game';
  * @param {object} req
  * @param {object} res
  */
-const createGameLogs = (req, res) => {
-// save game if user is authenticated
+export const createGameLogs = (req, res) => {
+  // save game if user is authenticated
   if (req.decoded && req.params.id) {
     const game = new Game(req.body);
     game.userID = req.decoded._id;
     game.gameID = req.params.id;
     game.save((error) => {
-      if (error) return res.status(400).send({ message: 'Fail... game logs not saved' });
-      return res.status(201).send({ message: 'Success... game logs saved' });
+      if (error) {
+        return res.status(400).send({
+          message: 'Fail... game logs not saved'
+        });
+      }
+      return res.status(201).send({
+        message: 'Success... game logs saved'
+      });
     });
   }
 };
 
-export default createGameLogs;
+/**
+ * Sorted winners function
+ * @returns {Object} array showing the winners and the frequency of wins
+ * @param {Object} gameWinners
+ */
+const sortWinners = (gameWinners) => {
+  const sortedWinners = {};
+  gameWinners.forEach((index) => {
+    sortedWinners[index] = (sortedWinners[index] || 0) + 1;
+  });
+  return sortedWinners;
+};
+
+/**
+ * getGameWinners function
+ * @returns {Object} array of the individual game winners
+ * @param {Object} gameResults
+ */
+const getGameWinners = (gameResults) => {
+  const gameWinners = [];
+  gameResults.forEach((gameResult) => {
+    gameWinners.push(gameResult.gameWinner);
+  });
+  return gameWinners;
+};
+
+/**
+ * getLeaderBoard function
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {Object} game response
+ */
+export const getLeaderboard = (req, res) => {
+  Game.aggregate([{
+    $group: {
+      _id: '$gameID',
+      gameID: {
+        $first: '$gameID'
+      },
+      gameWinner: {
+        $first: '$gameWinner'
+      },
+      gamePlayers: {
+        $first: '$gamePlayers'
+      }
+    }
+  }], (error, gameResults) => {
+    if (error) {
+      res.json({
+        message: 'Game leaderboard not successfully retrieved',
+        success: false,
+        error
+      });
+      return;
+    }
+    if (gameResults.length === 0) {
+      res.json({
+        message: 'No leaderboard available',
+        success: true
+      });
+      return;
+    }
+    const gameWinners = getGameWinners(gameResults);
+    const leaderboard = sortWinners(gameWinners);
+    res.send(200, {
+      message: 'Game leaderboard successfully retrieved',
+      success: true,
+      leaderboard
+    });
+  });
+};
